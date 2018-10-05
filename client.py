1a6f127eaef454cb8b563a001a781f82225a8b80
gdkat <gkatz97@gmail.com>
2018-10-05 14:18:27 -0400

    RS server and Client Implementation

--- client.py
+++ client.py
@@ -1,30 +1,67 @@
 #Client
-#[ first socket]
-try:
-    ctors=mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
+import socket as mysoc
+import pickle
 
-except mysoc.error as err:
-    print('{} \n'.format("socket open error ",err))
+def client():
+    #[ rs socket]
+    try:
+        ctors=mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
 
-#[second socket]
-try:
-    ctots=mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
+    except mysoc.error as err:
+        print('{} \n'.format("socket open error ",err))
 
-except mysoc.error as err:
-    print('{} \n'.format("socket open error ", err))
+    #[ts socket]
+    try:
+        ctots=mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
 
-[determine hostname of RS server and port ]
-[bind ctors socket to RS address, rsport]
+    except mysoc.error as err:
+        print('{} \n'.format("socket open error ", err))
 
-#First Connect to RS server
-ctors.send(“hostname”,RSserver )
-dr=ctors.recv(…..)
-if flag field(dr) == ‘A’
-    output(dr)
-else
-    #[connect to TS server]
-    if flag-field (dr) == ‘NS’
-        TSname= hostname-field(dr)
-        ctots.send(hostname) #[Determine IP address of TSname, bind ctots socket to TS address, tsport]
-        dr=ctots.recv(…..) #[Connect and send hostname string ]
-        output (dr)
+    #load file with hostname information to be resolved
+    try:
+        #fname = input("Enter file to read (Ex: foo.txt): ")
+        fname = "PROJI-HNS.txt"
+        fr = open(fname, "r")
+    except IOError as err:
+        print('{} \n'.format("File Open Error ",err))
+        print("Please ensure desired file to reverse exists in source folder and is named HW1test.txt")
+        exit()
+
+    #[determine hostname of RS server and port ]
+    sa_sameas_myaddr = mysoc.gethostbyname(mysoc.gethostname())
+    # Define the port on which you want to connect to the server
+    port = 50007
+    #[bind ctors socket to RS address, RS port]
+    try:
+        server_binding=(sa_sameas_myaddr,port)
+        ctors.connect(server_binding)
+    except mysoc.error as err:
+        print('{} \n'.format("connect error "), err)
+        exit()
+
+    #check rs-server.py for information on how data
+    #is being transferred and stored
+    #write all resolved IP information to RESOLVED.txt
+    with open("RESOLVED.txt", "w") as fw:
+        for hostname in fr:
+            ctors.send(hostname.strip().encode('utf-8'))
+            dataFromRS=ctors.recv(100)
+            if not dataFromRS: break
+            result=pickle.loads(dataFromRS)
+            if 'A' in result[1]:
+                fw.write(hostname.strip()+' '+result[0]+' '+result[1]+'\n')
+            """ else
+                #[connect to TS server]
+                if flag-field (dr) == ‘NS’
+                    TSname= hostname-field(dr)
+                    ctots.send(hostname) #[Determine IP address of TSname, bind ctots socket to TS address, tsport]
+                    dr=ctots.recv(…..) #[Connect and send hostname string ]
+                    output (dr) """
+
+    # close everything
+    fr.close()
+    fw.close()
+    ctors.close()
+    ctots.close()
+
+client()
--- ts-server.py
+++ ts-server.py
@@ -1,16 +1,50 @@
 #TS server
-try:
-    tssd = mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
-except mysoc.error as err:
-    print('[TS]: {}\n'.format("socket open error ", err))
+import socket as mysoc
+import pickle
 
-determine local hostname, IP
-address , select a port number
-tsdd.bind(…)
-ctsd,addr=tsdd.accept()
-hnstring=ctsd.recv(…)
-if hnstring in TS_table:
-    entry=TS_table(hnstring)
-else:
-    entry= “hname” + “Error: Host not found”
-    ctsd.send(entry)
+def tokenizer(entry):
+    for token in entry.split():
+            yield token
+
+def ts():
+    try:
+        tssd = mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
+
+    except mysoc.error as err:
+        print('[TS]: {}\n'.format("socket open error ", err))
+
+    #load table from server
+    try:
+        #fname = input("Enter file to read (Ex: foo.txt): ")
+        fname = "PROJI-DNSTS.txt"
+        fr = open(fname, "r")
+    except IOError as err:
+        print('{} \n'.format("File Open Error ",err))
+        print("Please ensure desired file to reverse exists in source folder and is named HW1test.txt")
+        exit()
+
+    TS_table = {}
+
+    for line in fr:
+        tokenize = tokenizer(line)
+        key = next(tokenize)
+        TS_table[key] = [next(tokenize),next(tokenize)]
+ 
+    #determine local hostname, IP
+    #address , select a port number
+    server_binding=('',50008)
+    tssd.bind(server_binding)
+    ctsd,addr=tssd.accept()
+
+    while true:
+        hnstring=ctsd.recv(100)
+        entry
+        if hnstring in TS_table:
+            entry= TS_table(hnstring)
+        else:
+            entry= “hname” + “Error: Host not found”
+        ctsd.send(pickle.dump(entry))
+
+    # close everything
+    fr.close()
+    tssd.close()
